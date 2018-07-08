d9996c5a667b4314a987485e1c1396d3b67c06f6
manish <manis.kasyap@gmail.com>
2018-07-08 23:23:39 +0400

    to test travis build

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        this will contain stuff
+        this will contain stuff - coming soon !!!
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
