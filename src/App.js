c1f1090e29bd650b2eabecaaab1ed3cda715ec62
manish <manis.kasyap@gmail.com>
2018-07-14 15:28:31 +0400

    using basic layout from antd

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 // import Button from "antd/lib/button";
 // import { Button } from "antd";
+import { Layout } from "antd";
 import "./App.css";
+
+const { Header, Footer, Sider, Content } = Layout;
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         this will contain stuff - coming soon !!!
+        <Layout>
+          <Header>header</Header>
+          <Layout>
+            <Sider>left sidebar</Sider>
+            <Content>main content</Content>
+            <Sider>right sidebar</Sider>
+          </Layout>
+          <Footer>footer</Footer>
+        </Layout>
         {/* <Button type="primary">Button</Button> */}
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
