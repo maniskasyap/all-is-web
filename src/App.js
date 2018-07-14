4c304d1811570ab6b93d5fae3df49c0f5f71c1d0
manish <manis.kasyap@gmail.com>
2018-07-14 14:24:52 +0400

    adding and configuring antd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
+// import Button from "antd/lib/button";
+// import { Button } from "antd";
 import "./App.css";
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         this will contain stuff - coming soon !!!
+        {/* <Button type="primary">Button</Button> */}
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
