111c831dcb88ec2ea789289f4a3e163f3586ea15
manish <manis.kasyap@gmail.com>
2018-07-14 19:27:53 +0400

    minor style changes

--- src/mk-intro/mk-intro.js
+++ src/mk-intro/mk-intro.js
@@ -4,12 +4,15 @@ import "./mk-intro.css";
 class MkIntro extends Component {
   render() {
     return (
-      <div className="intro flex flex-col flex-center flex-center-90">
-        <div className="intro-text">
-          Hello, I'm <span className="intro-text__name"> Manish</span>
+      <section id="home">
+        <div className="canvas"></div>
+        <div className="intro flex flex-col flex-center flex-center-90">
+          <div className="intro-text">
+            Hello, I'm <span className="intro-text__name"> Manish</span>
+          </div>
+          <div>I'm a full-stack web developer.</div>
         </div>
-        <div>I'm a full-stack web developer.</div>
-      </div>
+      </section>
     );
   }
 }
