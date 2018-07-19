f4224613b6d3e6b46f2d27d12dd364776ff36651
manish <manis.kasyap@gmail.com>
2018-07-19 19:07:30 +0400

    adding slider

--- src/mk-expert/mk-expert.js
+++ src/mk-expert/mk-expert.js
@@ -7,12 +7,18 @@ class MkExpert extends Component {
   render() {
     return (
       <section id="expert">
-        <MkScrollPos>{posY => <h1>{posY}</h1>}</MkScrollPos>
-        {/* <div className="flex">
-          <div className="one">one</div>
-          <div className="two">two</div>
-          <div className="three">three</div>
-        </div> */}
+        <MkScrollPos>
+          {posY => (
+            <div className={"expert-items " + (posY > 0 ? "fixed" : "")}>
+              {/* <div className="flex"> */}
+                {/* <h1>{posY}</h1> */}
+                <div style={{top: posY}} className="expert-item item-1">one</div>
+                <div style={{top: posY}} className="expert-item item-2">two</div>
+                <div style={{top: posY}} className="expert-item item-3">three</div>
+              {/* </div> */}
+            </div>
+          )}
+        </MkScrollPos>
       </section>
     );
   }
